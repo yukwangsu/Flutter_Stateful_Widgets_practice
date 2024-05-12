d6e3ab0ce38b6818b40f14e8e6b3349df6a9efa2
yukwangsu <86365727+yukwangsu@users.noreply.github.com>
2024-05-12 17:10:44 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_stateful_widgets_practice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_stateful_widgets_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void onClicked() {
+    //setState: State클래스에게 데이터가 변경되었다고 알리는 함수
+    //Dart가 build를 다시 한번 더 실행하고 렌더링한다.
+    setState(() {
+      counter = counter + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click Count',
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                //counter 변수의 값을 text로 표시
+                '$counter',
+                style: const TextStyle(fontSize: 30),
+              ),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: const Icon(
+                  Icons.add_box_rounded,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
